0fe2eb9bb57e51c03eadf0016868cd1b4073b290
leon0128 <leon0128>
2019-11-27 12:45:35 +0900

    [fix] Changed to pass arguments to the compiler as is

--- test.sh
+++ test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [ ! -e Compiler ]; then
-    echo "test-error: Compiler does not exist."
+    echo "test-err: Compiler does not exist."
     exit 1
 fi
 
-./Compiler
+./Compiler $@
 
 if [ $? -ne 0 ]; then
-    echo "test-error: failed to compile."
+    echo "test-err: failed to compile."
     exit 1
 fi
