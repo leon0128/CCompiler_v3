4bb381242adea102e3ccb737123dc66f137ce86a
leon0128 <leon0128>
2019-11-27 11:31:46 +0900

    [update] Implementation of FileManager::read()

--- src/file_manager.cpp
+++ src/file_manager.cpp
@@ -6,11 +6,33 @@ void FileManager::write(const char* filename,
     std::ofstream file(filename, std::ios::out);
     if(!file.is_open())
     {
-        std::cerr << "output-err: failed to open file: "
-                  << filename << std::endl;
+        openError(filename);
         return;
     }
 
     file << data;
     file.close();
+}
+
+void FileManager::read(const char* filename,
+                       std::string& data)
+{
+    std::ifstream file(filename, std::ios::in);
+    if(!file.is_open())
+    {
+        openError(filename);
+        return;
+    }
+
+    std::stringstream stream;
+    stream << file.rdbuf();
+
+    data = stream.str();
+    file.close();
+}
+
+void FileManager::openError(const char* filename)
+{
+    std::cerr << "file-err: failed to open file "
+              << "( " << filename << " )." << std::endl;  
 }
--- src/file_manager.hpp
+++ src/file_manager.hpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 
 class FileManager
@@ -9,6 +10,14 @@ class FileManager
 public:
     FileManager() = delete;
 
+    // 書き込み
     static void write(const char* filename,
                       const char* data);
+
+    // 読み込み
+    static void read(const char* filename,
+                     std::string& data);
+
+private:
+    static void openError(const char* filename);
 };
