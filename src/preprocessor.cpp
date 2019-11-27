78f1c06b6b7f3958e486c21bca7c7f7956eb7ef5
leon0128 <leon0128>
2019-11-27 12:39:42 +0900

    [update] Implemenation of Preprocessor::isValidArgument(), isOpenedFile(), isDeletedComment(), outputData(), preprocess()

--- src/preprocessor.cpp
+++ src/preprocessor.cpp
@@ -1,1 +1,101 @@
 #include "preprocessor.hpp"
+#include "file_manager.hpp"
+
+const char* Preprocessor::OUTPUT_FILENAME
+    = "tmp/preprocessor.log";
+
+Preprocessor::Preprocessor():
+    mSource(),
+    mIsValid(true)
+{
+}
+
+Preprocessor::~Preprocessor()
+{
+}
+
+bool Preprocessor::preprocess(int argc, char** argv,
+                              std::string& source)
+{
+    if(!isValidArgument(argc))
+        return mIsValid;
+    if(!isOpenedFile(argv))
+        return mIsValid;
+    if(!isDeletedComment())
+        return mIsValid;
+    
+    outputData();
+    
+    source.swap(mSource);
+    return mIsValid;
+}
+
+bool Preprocessor::isValidArgument(int argc)
+{
+    if(argc < 2)
+    {
+        std::cerr << "prep-err: invalid command line argument size "
+                  << "(argument size >= 2)." << std::endl;
+        mIsValid = false;
+    }
+
+    return mIsValid;
+}
+
+bool Preprocessor::isOpenedFile(char** argv)
+{
+    if(!FileManager::read(argv[1], mSource))
+    {
+        std::cerr << "prep-err: cannot open file "
+                  << "( " << argv[1] << " )" << std::endl;
+        mIsValid = false;
+    }
+
+    return mIsValid;
+}
+
+bool Preprocessor::isDeletedComment()
+{
+    std::size_t beginPos = std::string::npos;
+    std::size_t endPos   = std::string::npos;
+
+    // // コメントを 置換
+    while((beginPos = mSource.find("//")) != std::string::npos)
+    {
+        endPos = mSource.find("\n", beginPos);
+
+        size_t count
+            = (endPos != std::string::npos)
+                ? (endPos - beginPos) : (mSource.size() - 1);
+        mSource.replace(beginPos,
+                        count,
+                        "");
+    }
+
+    // /* *** */ コメントの置換
+    while((beginPos = mSource.find("/*")) != std::string::npos)
+    {
+        endPos = mSource.find("*/", beginPos + 2);
+
+        if(endPos != std::string::npos)
+        {
+            mSource.replace(beginPos,
+                            endPos + 2 - beginPos,
+                            " ");
+        }
+        else
+        {
+            std::cerr << "prep-err: unterminated \'/* *** */\' comment." << std::endl;
+            mIsValid = false;
+            break;
+        }
+    }
+
+    return mIsValid;
+}
+
+void Preprocessor::outputData() const
+{
+    FileManager::write(OUTPUT_FILENAME,
+                       mSource.c_str());
+}
--- src/preprocessor.hpp
+++ src/preprocessor.hpp
@@ -1,6 +1,34 @@
 #pragma once
 
+/* 実装状況
+* - argv[1] のファイルの対してのみ処理
+* - コメント の 削除
+*/
+
+#include <string>
+#include <iostream>
+
 class Preprocessor
 {
+private:
+    static const char* OUTPUT_FILENAME; // プリプロセスの結果を出力するファイル名
 
+public:
+    Preprocessor();
+    ~Preprocessor();
+
+    // 第一引数, 第二引数は コマンドライン引数
+    // 第三引数は 処理したソースコード
+    bool preprocess(int argc, char** argv,
+                    std::string& source);
+
+private:
+    // preprocess() で使用される関数
+    bool isValidArgument(int argc);
+    bool isOpenedFile(char** argv);
+    bool isDeletedComment();
+    void outputData() const;
+
+    std::string mSource;
+    bool mIsValid;
 };
