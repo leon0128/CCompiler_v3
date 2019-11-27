3646f8868ed5eaedbc8a4a2eb165b84a84626953
leon0128 <leon0128>
2019-11-27 10:00:39 +0900

    [update] Make exit status subject to evaluation

--- test.sh
+++ test.sh
@@ -4,3 +4,10 @@ if [ ! -e Compiler ]; then
     echo "test-error: Compiler does not exist."
     exit 1
 fi
+
+./Compiler
+
+if [ $? -ne 0 ]; then
+    echo "test-error: failed to compile."
+    exit 1
+fi
