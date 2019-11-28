a4c9629a320c12b2db08fccd147b1be6fa390a63
leon0128 <leon0128cocoa@gmail.com>
2019-11-28 17:37:03 +0900

    [update] Creating a helper class that easily calls instruction() in a Generator class

---
+++ src/operand.cpp
@@ -0,0 +1,51 @@
+#include "operand.hpp"
+
+const std::unordered_map<Operand::ERegister, const char*> Operand::REGISTER_NAME_MAP
+    = {{Operand::RAX,     "rax"}, {Operand::RBX,     "rbx"}, {Operand::RCX,     "rcx"}, {Operand::RDX,     "rdx"},
+       {Operand::RSI,     "rsi"}, {Operand::RDI,     "rdi"}, {Operand::RSP,     "rsp"}, {Operand::RBP,     "rbp"},
+       {Operand::R8,       "r8"}, {Operand::R9,       "r9"}, {Operand::R10,     "r10"}, {Operand::R11,     "r11"},
+       {Operand::R12,     "r12"}, {Operand::R13,     "r13"}, {Operand::R14,     "r14"}, {Operand::R15,     "r15"},
+       {Operand::EAX,     "eax"}, {Operand::EBX,     "ebx"}, {Operand::ECX,     "ecx"}, {Operand::EDX,     "edx"},
+       {Operand::ESI,     "esi"}, {Operand::EDI,     "edi"}, {Operand::ESP,     "esp"}, {Operand::EBP,     "ebp"},
+       {Operand::R8D,     "r8d"}, {Operand::R9D,     "r9d"}, {Operand::R10D,   "r10d"}, {Operand::R11D,   "r11d"},
+       {Operand::R12D,   "r12d"}, {Operand::R13D,   "r13d"}, {Operand::R14D,   "r14d"}, {Operand::R15D,   "r15d"},
+       {Operand::AX,       "ax"}, {Operand::BX,       "bx"}, {Operand::CX,       "cx"}, {Operand::DX,       "dx"},
+       {Operand::SI,       "si"}, {Operand::DI,       "di"}, {Operand::SP,       "sp"}, {Operand::BP,       "bp"},
+       {Operand::R8W,     "r8w"}, {Operand::R9W,     "r9w"}, {Operand::R10W,   "r10w"}, {Operand::R11W,   "r11w"},
+       {Operand::R12W,   "r12w"}, {Operand::R13W,   "r13w"}, {Operand::R14W,   "r14w"}, {Operand::R15W,   "r15w"},
+       {Operand::AH,       "ah"}, {Operand::BH,       "bh"}, {Operand::CH,       "ch"}, {Operand::DH,       "dh"},
+       {Operand::AL,       "al"}, {Operand::BL,       "bl"}, {Operand::CL,       "cl"}, {Operand::DL,       "dl"},
+       {Operand::SIL,     "sil"}, {Operand::DIL,     "dil"}, {Operand::SPL,     "spl"}, {Operand::BPL,     "bpl"},
+       {Operand::R8L,     "r8l"}, {Operand::R9L,     "r9l"}, {Operand::R10L,   "r10l"}, {Operand::R11L,   "r11l"},
+       {Operand::R12L,   "r12l"}, {Operand::R13L,   "r13l"}, {Operand::R14L,   "r14l"}, {Operand::R15L,   "r15l"},
+       {Operand::R0,       "r0"}, {Operand::R1,       "r1"}, {Operand::R2,       "r2"}, {Operand::R3,       "r3"},
+       {Operand::R4,       "r4"}, {Operand::R5,       "r5"}, {Operand::R6,       "r6"}, {Operand::R7,       "r7"},
+       {Operand::MM0,     "mm0"}, {Operand::MM1,     "mm1"}, {Operand::MM2,     "mm2"}, {Operand::MM3,     "mm3"},
+       {Operand::MM4,     "mm4"}, {Operand::MM5,     "mm5"}, {Operand::MM6,     "mm6"}, {Operand::MM7,     "mm7"},
+       {Operand::XMM0,   "xmm0"}, {Operand::XMM1,   "xmm1"}, {Operand::XMM2,   "xmm2"}, {Operand::XMM3,   "xmm3"},
+       {Operand::XMM4,   "xmm4"}, {Operand::XMM5,   "xmm5"}, {Operand::XMM6,   "xmm6"}, {Operand::XMM7,   "xmm7"},
+       {Operand::XMM8,   "xmm8"}, {Operand::XMM9,   "xmm9"}, {Operand::XMM10, "xmm10"}, {Operand::XMM11, "xmm11"},
+       {Operand::XMM12, "xmm12"}, {Operand::XMM13, "xmm13"}, {Operand::XMM14, "xmm14"}, {Operand::XMM15, "xmm15"},
+       {Operand::YMM0,   "ymm1"}, {Operand::YMM1,   "ymm1"}, {Operand::YMM2,   "ymm2"}, {Operand::YMM3,   "ymm3"},
+       {Operand::YMM4,   "ymm4"}, {Operand::YMM5,   "ymm5"}, {Operand::YMM6,   "ymm6"}, {Operand::YMM7,   "ymm7"},
+       {Operand::YMM8,   "ymm8"}, {Operand::YMM9,   "ymm9"}, {Operand::YMM10, "ymm10"}, {Operand::YMM11, "ymm11"},
+       {Operand::YMM12, "ymm12"}, {Operand::YMM13, "ymm13"}, {Operand::YMM14, "ymm14"}, {Operand::YMM15, "ymm15"}};
+
+Operand::Operand():
+    mString()
+{
+}
+
+Operand::Operand(long value):
+    mString(std::to_string(value))
+{
+}
+
+Operand::Operand(ERegister reg):
+    mString(REGISTER_NAME_MAP.at(reg))
+{
+}
+
+Operand::~Operand()
+{
+}
---
+++ src/operand.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <string>
+#include <unordered_map>
+
+class Operand
+{
+private:
+    enum ERegister
+    {
+        // 64 bit
+        RAX, RBX, RCX, RDX,
+        RSI, RDI, RSP, RBP,
+        R8, R9, R10, R11, R12, R13, R14, R15,
+        // 32 bit
+        EAX, EBX, ECX, EDX,
+        ESI, EDI, ESP, EBP,
+        R8D, R9D, R10D, R11D, R12D, R13D, R14D, R15D,
+        // 16 bit
+        AX, BX, CX, DX,
+        SI, DI, SP, BP,
+        R8W, R9W, R10W, R11W, R12W, R13W, R14W, R15W,
+        // 上位 8 bit
+        AH, BH, CH, DH,
+        // 下位 8 bit
+        AL, BL, CL, DL,
+        SIL, DIL, SPL, BPL,
+        R8L, R9L, R10L, R11L, R12L, R13L, R14L, R15L,
+        // FPU
+        R0, R1, R2, R3, R4, R5, R6, R7,
+        // MMX
+        MM0, MM1, MM2, MM3, MM4, MM5, MM6, MM7,
+        // XMM
+        XMM0, XMM1, XMM2, XMM3, XMM4, XMM5, XMM6, XMM7,
+        XMM8, XMM9, XMM10, XMM11, XMM12, XMM13, XMM14, XMM15,
+        // YMM
+        YMM0, YMM1, YMM2, YMM3, YMM4, YMM5, YMM6, YMM7,
+        YMM8, YMM9, YMM10, YMM11, YMM12, YMM13, YMM14, YMM15
+    };
+    static const std::unordered_map<ERegister, const char*> REGISTER_NAME_MAP;
+
+public:
+    Operand();
+    Operand(long value);
+    Operand(ERegister reg);
+    Operand(ERegister reg, long offset);
+
+    ~Operand();
+
+    const char* operator()(){return mString.c_str();}
+private:
+    std::string mString;
+};
