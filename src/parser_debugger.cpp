ffe582053aa80f4ca1e2dd99c95229e27c52c152
leon0128 <leon0128cocoa@gmail.com>
2019-11-30 00:08:05 +0900

    [update] Making the output of debug data of ParserDebugger class easier to see

--- src/parser_debugger.cpp
+++ src/parser_debugger.cpp
@@ -3,46 +3,96 @@
 #include "token.hpp"
 
 ParserDebugger::ParserDebugger():
-    mStream(),
-    mIndent(0)
+    mIsValidIndents()
+{
+}
+
+ParserDebugger::~ParserDebugger()
 {
 }
 
 bool ParserDebugger::debug(const char* filename,
                            Token* parent)
 {
-    consume(parent);
+    std::string data(consume(parent));
 
-    return FileManager::write(filename, mStream);
+    return FileManager::write(filename, data);
 }
 
-void ParserDebugger::consume(Token* token)
+std::string ParserDebugger::consume(Token* token, std::string disc)
 {
-    if(!token)
-        return;
+    std::string data;
 
-    if(token->isIntegral())
-        conIntegral(token);
-    else if(token->isOperator())
-        conOperator(token);
-    else
-        error(token);
+    if(token)
+    {
+        if(token->isOperator())
+            data = conOperator(token, disc);
+        else if(token->isIntegral())
+            data = conIntegral(token, disc);
+        else
+            error(token);
+    }
+
+    return data;
 }
-
-void ParserDebugger::conIntegral(Token* token)
+std::string ParserDebugger::conOperator(Token* token, std::string disc)
 {
-    IntegralToken* intTok
-        = Token::cast<IntegralToken*>(token);
+    std::stringstream stream;
+    addNodeHeader(token, stream, disc);
     
-
-}
-
-void ParserDebugger::conOperator(Token* token)
-{
+    #if DEBUG_OPERATOR
     OperatorToken* opeTok
         = Token::cast<OperatorToken*>(token);
+    mIsValidIndents.push_back(true);
+    stream << createIndent(mIsValidIndents.size() - 1)
+           << " |--"
+           << consume(opeTok->lhs, "lhs");
+    mIsValidIndents.back() = false;
+    stream << createIndent(mIsValidIndents.size() - 1)
+           << " `--"
+           << consume(opeTok->rhs, "rhs");
+    mIsValidIndents.pop_back();
+    #endif
 
+    std::string data(stream.str());
+    return data;
+}
 
+std::string ParserDebugger::conIntegral(Token* token, std::string disc)
+{    
+    std::stringstream stream;
+    addNodeHeader(token, stream, disc);
+
+    #if DEBUG_INTEGRAL
+    IntegralToken* intTok
+        = Token::cast<IntegralToken*>(token);
+    stream << createIndent(mIsValidIndents.size())
+           << " `----- "
+           << intTok->value
+           << std::endl;
+    #endif
+
+    std::string data(stream.str());
+    return data;
+}
+
+void ParserDebugger::addNodeHeader(Token* token,
+                                   std::stringstream& stream,
+                                   std::string& disc)
+{
+    stream << "[-] "
+           << Token::KIND_NAME_MAP.at(token->kind)
+           << " (" << disc << ")"
+           << std::endl;
+}
+
+std::string ParserDebugger::createIndent(std::size_t size)
+{
+    std::string data;
+    for(std::size_t i = 0; i < size; i++)
+        data += mIsValidIndents.at(i) ? " |  " : "    ";
+    
+    return data;
 }
 
 bool ParserDebugger::error(Token* token)
@@ -50,4 +100,6 @@ bool ParserDebugger::error(Token* token)
     std::cerr << "debu-err: unimplemented type "
               << "( " << Token::KIND_NAME_MAP.at(token->kind) << " )."
               << std::endl;
+
+    return false;
 }
--- src/parser_debugger.hpp
+++ src/parser_debugger.hpp
@@ -1,24 +1,39 @@
 #pragma once
 
 #include <string>
+#include <vector>
 #include <sstream>
 #include <iostream>
+
+// 各トークンのメンバを表示するかどうか
+#define DEBUG_INTEGRAL 1
+#define DEBUG_OPERATOR 1
 
 class ParserDebugger
 {
 public:
     ParserDebugger();
+    ~ParserDebugger();
 
     bool debug(const char* filename,
                class Token* parent);
 
 private:
-    void consume(class Token* token);
-    void conOperator(class Token* token);
-    void conIntegral(class Token* token);
+    std::string consume(class Token* token,
+                        std::string disc = std::string());
+    std::string conOperator(class Token* token,
+                            std::string disc = std::string());
+    std::string conIntegral(class Token* token,
+                            std::string disc = std::string());
+
+    // [-] [KIND_NAME]\n をストリームに追加
+    void addNodeHeader(class Token* token,
+                       std::stringstream& stream,
+                       std::string& disc);
+    // indent の作成
+    std::string createIndent(std::size_t size);
 
     bool error(class Token* token);
 
-    std::stringstream mStream;
-    std::size_t mIndent;
+    std::vector<bool> mIsValidIndents;
 };
