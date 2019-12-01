e0f0fee7fd4a8ddaf636e0f4d3347bb1b5a7acc6
leon0128 <leon0128cocoa@gmail.com>
2019-12-01 14:31:06 +0900

    [update] Create parser helper class to manage functions and variables

---
+++ src/traits_manager.cpp
@@ -0,0 +1,72 @@
+#include "traits_manager.hpp"
+
+TraitsManager::TraitsManager():
+    mTraits(),
+    mScope(0)
+{
+}
+
+TraitsManager::~TraitsManager()
+{
+}
+
+void TraitsManager::decScope()
+{
+    while(1)
+    {
+        if(mTraits.back().scope == mScope)
+            mTraits.pop_back();
+        else
+            break;
+    }
+
+    mScope--;
+}
+
+bool TraitsManager::addTrait(Token* token, Token::EType type)
+{
+    VariableToken* varTok
+        = Token::cast<VariableToken*>(token);
+
+    for(auto&& e : mTraits)
+    {
+        if(varTok->name == e.name)
+            return error(token, "variable overloading");
+    }
+
+    mTraits.push_back(Trait{mScope,
+                            type,
+                            varTok->name,
+                            (mTraits.size() + 1) * 8});
+    return setTrait(token);
+}
+
+bool TraitsManager::setTrait(Token* token) const
+{
+    VariableToken* varTok
+        = Token::cast<VariableToken*>(token);
+
+    for(auto&& e : mTraits)
+    {
+        if(varTok->name == e.name)
+        {
+            varTok->type   = e.type;
+            varTok->offset = e.offset;
+            return true;
+        }
+    }
+
+    return error(token, "undefined variable");
+}
+
+bool TraitsManager::error(Token* token, const char* message) const
+{
+    VariableToken* varTok
+        = Token::cast<VariableToken*>(token);
+
+    std::cerr << "trai-err: " << message
+              << " ( " << varTok->name << " )."
+              << std::endl;
+            
+    return false;
+}
---
+++ src/traits_manager.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include "token.hpp"
+
+class TraitsManager
+{
+private:
+    friend class Parser;
+
+    TraitsManager();
+    ~TraitsManager();
+
+    struct Trait
+    {
+        long scope;
+        Token::EType type;
+        std::string name;
+        long offset;
+    };
+
+    void incScope(){mScope++;}
+    void decScope();
+
+    bool addTrait(Token* token, Token::EType type);
+    bool setTrait(Token* token) const;
+
+    bool error(Token* token, const char* message) const;
+
+    std::vector<Trait> mTraits;
+    long mScope;
+};
